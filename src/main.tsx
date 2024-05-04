9ea75a7ca2dd8868e6f051d92b9a2c5a26e1133d
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-05-04 22:30:11 +0545

    set the dnd5eapi as default base url for axios

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import Providers from "./lib/Providers.tsx";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://www.dnd5eapi.co/api";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
