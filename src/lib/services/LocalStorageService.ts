7e6e833d081e9ac86659398681d3995bf073b99d
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-05-06 21:42:23 +0545

    add local storage service

---
+++ src/lib/services/LocalStorageService.ts
@@ -0,0 +1,18 @@
+export class LocalStorageService {
+  static set(key: string, data: any) {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+
+  static get<T>(key: string): T | undefined {
+    const rawString = localStorage.getItem(key);
+    if (rawString) return JSON.parse(rawString);
+  }
+
+  static remove(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  static clear() {
+    localStorage.clear();
+  }
+}
