0f9c80946790582e482d0740711cd3b1e69840c9
Keshav Kishor Ram <ramabishek40@gmail.com>
2024-05-07 19:47:39 +0545

    add a dropdown component

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+export type DropdownOption = {
+  label: string;
+  value: string;
+};
+
+type Props = {
+  title: string;
+  options: DropdownOption[];
+  onChange?: React.ChangeEventHandler<HTMLSelectElement>;
+};
+
+const Dropdown = ({ title, options, onChange }: Props) => {
+  return (
+    <Root>
+      <label htmlFor="dropdown">{title}</label>
+      <StyledSelect id="dropdown" onChange={onChange} defaultValue={"default"}>
+        {options.map((option) => (
+          <StyledOption key={option.value} value={option.value}>
+            {option.label}
+          </StyledOption>
+        ))}
+      </StyledSelect>
+    </Root>
+  );
+};
+
+export default Dropdown;
+
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  color: #fefbf6;
+`;
+const StyledSelect = styled.select`
+  background-color: transparent;
+  padding: 0.6rem 0.8rem;
+  cursor: pointer;
+  border-radius: 4px;
+  color: #fefbf6;
+
+  &:focus {
+    outline: 1px solid #ccc;
+  }
+`;
+
+const StyledOption = styled.option`
+  padding: 10px 15px;
+`;
